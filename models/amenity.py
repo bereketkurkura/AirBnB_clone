063d61f531e623f513f32f156df604789966363e
Henok Belay <105018369+HENOKB123@users.noreply.github.com>
2022-10-29 20:54:10 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,8 @@
-amenity.py
+#!/usr/bin/python3
+"""Contains the Amenity model"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Implements the Amenity model"""
+    name = ""
